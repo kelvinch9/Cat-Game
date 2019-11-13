1f222d28bbec539be55cc5859406535fa555e457
mordway1 <45020262+mordway1@users.noreply.github.com>
2019-11-12 21:20:59 -0500

    GameState methods and JavaDocs

--- src/application/GameState.java
+++ src/application/GameState.java
@@ -1,7 +1,50 @@
 package application;
 
-public class GameState {
+public class GameState extends Application {
+	
+	// game over instance variable?
+	// needs a constructor?
+	private boolean shouldReplay;
+	
+	/**
+	 * Creates the intro scene for the game. The intro scene
+	 * will have instructions on how to play the game (up to jump, down to duck). 
+	 * It will have a "Start" button to click and a "Credits" button to see the game
+	 * creators.
+	 * 
+	 */
+	public void setIntroScene() {
+		;
+	}
+	
+	
+	
+	/**
+	 * Creates the start scene for the game. The start scene will display
+	 * the cat and the first scene with obstacles and a '0' score. It will say press "space"
+	 * to start the game.  
+	 * 
+	 * 
+	 */
+	public void setGameStartScene() {
+		;
+	}
+	
+	
+	
+	/**
+	 * Creates the game end scene for the game. Once that player dies (collide with object),
+	 * the GameEndScene will display. This scene displays the current score, the high score,
+	 * and will prompt the user whether to replay the game.
+	 * 
+	 * Note: High score will only be saved for the next round if the game is replayed.
+	 * 
+	 * 
+	 */
+	public void setGameEndScene() {
+		;
+	}
 
-	// for Matt!
 
 }
+
