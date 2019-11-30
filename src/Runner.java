a969db0fe0c27acbfe71d197ee26dd965e5e12e2
mordway1 <mordway@yipitdata.com>
2019-11-30 14:53:14 -0500

    adding multiple JPanels and JButton for game start. KeyListener issue

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,12 @@
+import java.awt.CardLayout;
 import java.awt.EventQueue;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * This class runs the game. It extends the JFrame class
@@ -23,7 +30,52 @@ public class Runner extends JFrame{
 	 */
 	private void initUI() {
 		
-		add(new Stage());
+// -------- BEGIN CODE -------		
+		// Create CardLayout Panel to switch between "cards" (diff. screens)
+		CardLayout cards = new CardLayout();
+		JPanel UI = new JPanel();
+		UI.setLayout(cards);
+	
+		
+//		contentPane.setLayout(cardLayout);
+		
+		// Create Panel for Start of Game
+		Panel startPanel = new Panel();
+		JButton start = new JButton("start");		
+		startPanel.add(start);
+		
+		
+		// Create Panel for Game
+		Panel gamePanel = new Panel();
+		Stage game = new Stage();
+		gamePanel.add(game);
+		
+		
+		// UI - add panels to UI Main panel
+		UI.add("2", startPanel);
+		UI.add("3", gamePanel);
+		this.setContentPane(UI);
+		cards.show(UI, "1");
+		
+		// add action for start button - When you click start button, it goes to 
+		// next panel which is gamePanel
+		start.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cards.next(UI);
+				
+			}
+			
+		});
+		
+		
+		
+// --------- END - TEST OF JPANELS --------- 		
+		
+		
+		// COMMENTED OUT
+//		add(new Stage());
 		setResizable(false);
 		pack(); // automatically sets size of JFrame
 		setTitle("Cat Game");
