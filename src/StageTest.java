fb9b94fe81ce4cb0a71829f74d35b76bcce51ec5
kelvinch9 <57468537+kelvinch9@users.noreply.github.com>
2019-12-07 18:19:35 -0500

    Update StageTest.java

--- src/StageTest.java
+++ src/StageTest.java
@@ -28,6 +28,45 @@ class StageTest {
 		assertEquals(stage.getBird().getY(), 160);
 
 	}
+	
+	@Test
+	void testCalcScore() {
+		assertEquals(score.calcScore(0, 0), 0);
+		assertEquals(score.calcScore(10, 572), 1572);
+		assertEquals(score.calcScore(100, 100), 10100);
+		assertEquals(score.calcScore(10, 0), 1000);
+		assertEquals(score.calcScore(0, 600), 600);
+	}
+
+	@Test
+	void testCalcHighScore() {
+		score.setHighScore(100);
+		score.calcHighScore(10);
+		assertEquals(100, score.getHighScore());
+		score.calcHighScore(1000);
+		assertEquals(1000, score.getHighScore());
+	}
+
+	
+	@Test
+	void testDisplayHighScore() {
+		score.setIfGameRetried(false);
+		score.setHighScore(100);
+		assertEquals(("High Score: " + score.getHighScore() +
+				"\nIt's your first time. You get the high score!"), 
+			score.displayHighScore(100));
+		score.setIfGameRetried(true);
+		score.setPrevHighScore(100);
+		assertEquals(("High Score: " + score.getHighScore() +
+				"\nNice try! You tied the high score."), 
+			score.displayHighScore(100));
+		assertEquals(("High Score: " + score.getHighScore() +
+				"\nCongrats! New High Score!"), 
+				score.displayHighScore(1000));
+		assertEquals(("High Score: " + score.getHighScore() +
+				"\nBetter luck next time!"), 
+			score.displayHighScore(1));
+	}
 
 	@Test
 	void testBoxCollision() {
