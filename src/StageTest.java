063206657818376688a9decc55c12e410a654c83
mordway1 <mordway@yipitdata.com>
2019-12-07 21:04:25 -0500

    update JUnit tests

--- src/StageTest.java
+++ src/StageTest.java
@@ -17,57 +17,20 @@ class StageTest {
 	@Test
 	void testInitStage() {
 		stage.initStage();
-		assertEquals((stage.getBoxes().get(0).getX()), 400);
-		assertEquals((stage.getBoxes().get(0).getY()), 250);
-		assertEquals(stage.getCoins().get(0).getX(), 250);
-		assertEquals(stage.getCoins().get(0).getY(), 210);
-		assertEquals(stage.getCat().getX(), 40);
-		assertEquals(stage.getCat().getY(), 250);
-		assertEquals(stage.getGhosts().get(0).getX(), 600);
-		assertEquals(stage.getGhosts().get(0).getY(), 210);
-		assertEquals(stage.getBird().getX(), 400);
-		assertEquals(stage.getBird().getY(), 160);
+		assertEquals(400, (stage.getBoxes().get(0).getX()));
+		assertEquals(250, (stage.getBoxes().get(0).getY()));
+		assertEquals(250, stage.getCoins().get(0).getX());
+		assertEquals(210, stage.getCoins().get(0).getY());
+		assertEquals(40, stage.getCat().getX());
+		assertEquals(250, stage.getCat().getY());
+		assertEquals(600, stage.getGhosts().get(0).getX());
+		assertEquals(210, stage.getGhosts().get(0).getY());
+		assertEquals(400, stage.getBird().getX());
+		assertEquals(160, stage.getBird().getY());
 
-	}
-
-	@Test
-	void testCalcScore() {
-		assertEquals(score.calcScore(0, 0), 0);
-		assertEquals(score.calcScore(10, 572), 1572);
-		assertEquals(score.calcScore(100, 100), 10100);
-		assertEquals(score.calcScore(10, 0), 1000);
-		assertEquals(score.calcScore(0, 600), 600);
-	}
-
-	@Test
-	void testCalcHighScore() {
-		score.setHighScore(100);
-		score.calcHighScore(10);
-		assertEquals(100, score.getHighScore());
-		score.calcHighScore(1000);
-		assertEquals(1000, score.getHighScore());
 	}
 
 	
-	@Test
-	void testDisplayHighScore() {
-		score.setIfGameRetried(false);
-		score.setHighScore(100);
-		assertEquals(("High Score: " + score.getHighScore() +
-				"\nIt's your first time. You get the high score!"), 
-			score.displayHighScore(100));
-		score.setIfGameRetried(true);
-		score.setPrevHighScore(100);
-		assertEquals(("High Score: " + score.getHighScore() +
-				"\nNice try! You tied the high score."), 
-			score.displayHighScore(100));
-		assertEquals(("High Score: " + score.getHighScore() +
-				"\nCongrats! New High Score!"), 
-				score.displayHighScore(1000));
-		assertEquals(("High Score: " + score.getHighScore() +
-				"\nBetter luck next time!"), 
-			score.displayHighScore(1));
-	}
 	
 	@Test
 	void testBoxCollision() {
@@ -78,7 +41,8 @@ class StageTest {
 		Box box = new Box(100, Y);
 		stage.getBoxes().add(box);
 		stage.checkCollisions();
-		assertEquals(stage.getStageOfGame(), 2);
+		
+		assertEquals(2, stage.getStageOfGame());
 	}
 	
 	@Test
@@ -90,7 +54,8 @@ class StageTest {
 		Coin coin = new Coin(100, Y);
 		stage.getCoins().add(coin);
 		stage.checkCollisions();
-		assertEquals(stage.getCoinsCollected(), 1);
+		
+		assertEquals(1, stage.getCoinsCollected());
 	}
 	
 	@Test
@@ -102,7 +67,7 @@ class StageTest {
 		Ghost ghost = new Ghost(100, Y);
 		stage.getGhosts().add(ghost);
 		stage.checkCollisions();
-		assertEquals(stage.getCat().getGhostsCollected(), 1);
+		assertEquals(1, stage.getCat().getGhostsCollected());
 	}
 	
 	
